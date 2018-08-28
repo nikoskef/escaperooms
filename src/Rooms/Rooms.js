414dc3ad097d75e8b848739131a90a023f5352a8
nikoskef <nikoskef86@gmail.com>
2018-08-28 15:00:04 +0300

    Second

--- src/Rooms/Rooms.js
+++ src/Rooms/Rooms.js
@@ -6,7 +6,7 @@ import { Esrooms } from './Esrooms'
 const Room = ({ id, company, name, img }) => {
 	return (
 		<div className='tc dib br3 pa3 ma2 grow antonia bw2 shadow5'>
-			<img alt = {name} src = {img}/>
+			<img className=' br2 h4 w5 dib' alt = {name} src = {require(`../rimages/${img}`)}/>
 			<div className= 'white'>
 				<p>{company}</p>
 				<p>{name}</p>
